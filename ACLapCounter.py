7e9cd9d09b921aaef33333b6269b2ed9c9b1c27e
Harmon Amakobe <hamakobe@users.noreply.github.com>
2017-06-17 23:54:52 -0400

    Added top speed label
    Could not find easy way to add fuel, but played around with max top speed. Currently displays on console but does not update label.

--- ACLapCounter.py
+++ ACLapCounter.py
@@ -4,10 +4,12 @@ import acsys
 
 l_lapcount=0
 lapcount=0
+l_speed=0
+topspeed=0
 
 def acMain(ac_version):
 
-    global l_lapcount
+    global l_lapcount, l_fuel
     appWindow = ac.newApp("Lap Counter")
     
     ac.setSize(appWindow, 200, 200)
@@ -16,12 +18,22 @@ def acMain(ac_version):
     ac.console("Hello Assetto Corsa console!")
     
     l_lapcount = ac.addLabel(appWindow, "Laps: 0");
+    l_speed = ac.addLabel(appWindow, "Speed: 0");
     ac.setPosition(l_lapcount, 3, 30)
+    ac.setPosition(l_speed, 3, 60)
     
     return "Lap Counter"
+    
 def acUpdate(deltaT):
-    global l_lapcount, lapcount
+    global l_lapcount, lapcount, topspeed
     laps = ac.getCarState(0, acsys.CS.LapCount)
+    speed = ac.getCarState(0, acsys.CS.SpeedMPH)
+    
+    while speed > topspeed:
+        topspeed = speed
+        ac.console("{} MPH".format(topspeed))
+        ac.setText(l_speed, "Speed: {}".format(topspeed))
+    
     if laps > lapcount:
-        lapcount = laps        	        	
+        lapcount = laps
         ac.setText(l_lapcount, "Laps: {}".format(lapcount))
