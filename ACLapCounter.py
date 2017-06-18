9845a9ae86aad80244b6a3d594a92e166a7dac71
Harmon Amakobe <hamakobe@users.noreply.github.com>
2017-06-17 22:54:24 -0400

    Created working lap counter
    Followed the entire tutorial and ran a successful one lap test.

--- ACLapCounter.py
+++ ACLapCounter.py
@@ -2,8 +2,26 @@ import sys
 import ac
 import acsys
 
-def acMain(ac_version):
-	appWindow = ac.newApp("Lap Counter")
-	ac.setSize(appWindow, 200, 200)
-	return "Lap Counter"
+l_lapcount=0
+lapcount=0
 
+def acMain(ac_version):
+
+    global l_lapcount
+    appWindow = ac.newApp("Lap Counter")
+    
+    ac.setSize(appWindow, 200, 200)
+
+    ac.log("Hello, Assetto Corsa application world!")
+    ac.console("Hello Assetto Corsa console!")
+    
+    l_lapcount = ac.addLabel(appWindow, "Laps: 0");
+    ac.setPosition(l_lapcount, 3, 30)
+    
+    return "Lap Counter"
+def acUpdate(deltaT):
+    global l_lapcount, lapcount
+    laps = ac.getCarState(0, acsys.CS.LapCount)
+    if laps > lapcount:
+        lapcount = laps        	        	
+        ac.setText(l_lapcount, "Laps: {}".format(lapcount))
