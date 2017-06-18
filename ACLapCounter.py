b75e5b7d942483954ffa227f90cac184257e2283
Harmon Amakobe <hamakobe@users.noreply.github.com>
2017-06-18 14:08:55 -0400

    Fixed Top Speed not updating
    Turned out that l_speed had not been declared as a global variable and acUpdate could not access the actual label.

--- ACLapCounter.py
+++ ACLapCounter.py
@@ -9,7 +9,7 @@ topspeed=0
 
 def acMain(ac_version):
 
-    global l_lapcount, l_fuel
+    global l_lapcount, l_speed, l_fuel, topspeed
     appWindow = ac.newApp("Lap Counter")
     
     ac.setSize(appWindow, 200, 200)
@@ -17,23 +17,23 @@ def acMain(ac_version):
     ac.log("Hello, Assetto Corsa application world!")
     ac.console("Hello Assetto Corsa console!")
     
+    l_speed = ac.addLabel(appWindow, "Highest Top Speed: {}".format(topspeed));
     l_lapcount = ac.addLabel(appWindow, "Laps: 0");
-    l_speed = ac.addLabel(appWindow, "Speed: 0");
     ac.setPosition(l_lapcount, 3, 30)
     ac.setPosition(l_speed, 3, 60)
     
     return "Lap Counter"
     
 def acUpdate(deltaT):
-    global l_lapcount, lapcount, topspeed
+    global l_lapcount, l_speed, lapcount, topspeed
     laps = ac.getCarState(0, acsys.CS.LapCount)
     speed = ac.getCarState(0, acsys.CS.SpeedMPH)
     
-    while speed > topspeed:
-        topspeed = speed
-        ac.console("{} MPH".format(topspeed))
-        ac.setText(l_speed, "Speed: {}".format(topspeed))
-    
+    if speed > topspeed:
+        topspeed = round(speed,2)
+        ac.console("Highest Top Speed: {} MPH".format(topspeed))
+        ac.setText(l_speed, "Highest Top Speed: {}".format(topspeed))
+        
     if laps > lapcount:
         lapcount = laps
         ac.setText(l_lapcount, "Laps: {}".format(lapcount))
